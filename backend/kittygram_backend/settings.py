a52130f22e3bc150a246f5ac54620e4c8962449b
Comaash <comaash@gmail.com>
2023-08-03 10:44:04 +0000

    infra_1_try2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['158.160.71.233', '127.0.0.1', 'localhost', 'ash-kittygram.sytes.net']
 
